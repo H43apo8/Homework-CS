31c1dcd332fcb6ef5448eb0ab920ca37b2c93adb
H43apo8 <H43apo8@yandex.ru>
2022-12-03 17:04:42 +0300

    ДЗ Семинара №2 (задача №3)

---
+++ Task007/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6->да
+7->да
+1->нет*/
+Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+void CheckDayWeek(int dayWeek)
+{
+    if (dayWeek == 6 || dayWeek == 7)
+    {
+        Console.WriteLine("(Это выходной) -> да");
+    }
+    else if (dayWeek < 1 || dayWeek > 7)
+    {
+        Console.WriteLine("Мимо, попробуй еще разочек...");
+    }
+    else Console.WriteLine("(Это не выходной) -> нет");
+}
+
+CheckDayWeek(dayWeek);
