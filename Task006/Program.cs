d66e25e89c45e8a3afa2e5ec8d9dd08e4d783c7c
H43apo8 <H43apo8@yandex.ru>
2022-12-03 16:50:18 +0300

    ДЗ Семинара №2 (задача №2)

---
+++ Task006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645-> 5
+//78->третьей цифры нет
+//32679 -> 6
+Console.Clear();
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string txt = Convert.ToString(num);
+if (txt.Length > 2)
+{
+    Console.WriteLine("3я цифра = " + txt[2]);
+}
+else
+{
+    Console.WriteLine("3й цифры нет");
+}
