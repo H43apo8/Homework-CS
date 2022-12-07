aad5e89632a8b4254521a320b98bd6e36ee6d2dc
H43apo8 <H43apo8@yandex.ru>
2022-12-07 21:08:00 +0300

    ДЗ №3.2 "Длина отрезка в 3D"

---
+++ HomeWork#3/Task002/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B(2, 1, -7), -> 15.84
+A(7, -5, 0); B(1, -1, 9)-> 11.53*/
+
+Console.Clear();
+
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+int ReadInt(string message)
+{
+    Console.Write(message); 
+    return Convert.ToInt32(Console.ReadLine());
+}
