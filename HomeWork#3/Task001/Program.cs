be6f2623935fa701d5505e467351b3fcff5848c0
H43apo8 <H43apo8@yandex.ru>
2022-12-07 20:52:00 +0300

    ДЗ №3.1 "Проверка на Палиндром"

---
+++ HomeWork#3/Task001/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+Console.Clear();
+Console.WriteLine("Введите 5ти значное число: ");
+string num = Console.ReadLine();
+int l = num.Length;
+
+if (l == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"Палиндром - {num}");
+    }
+    else
+    {
+        Console.WriteLine($"Не палиндром - {num}");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА ввода: {num} не 5тизначное");
+}
