e99f966f7a7cb4fbbdecd104af48943e23017b07
H43apo8 <H43apo8@yandex.ru>
2022-12-10 14:41:17 +0300

    ДЗ №4.2 "Сумма цифр в числе"

---
+++ HomeWork#4/Task002/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Clear();
+int num = ReadInt("Введите число: ");
+int length = NumberLen(num);
+SumNumb(num, length);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumb(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе равна {sum}");
+}
