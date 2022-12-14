59f0a2b957715351bb2809d0b798b13d91ccf78c
H43apo8 <H43apo8@yandex.ru>
2022-12-14 21:52:37 +0300

    ДЗ №5.2 "Сумма элементов, стоящих на нечётных позициях."

---
+++ HomeWork#5/Task002/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19*/
+
+Console.Clear();
+int size = ReadInt("Размер массива: ");
+int min = ReadInt("Минимальное число: ");
+int max = ReadInt("Максимальное число: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        result += numbers[i];
+    }
+}
+Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
