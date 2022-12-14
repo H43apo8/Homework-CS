12e3d17e95e00173b2220222f5c40c6ccc92e851
H43apo8 <H43apo8@yandex.ru>
2022-12-14 21:58:31 +0300

    ДЗ №5.3 "Разницу между максимальным и минимальным элементов массива."

---
+++ HomeWork#5/Task003/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+Console.Clear();
+int size = ReadInt("Размер массива: ");
+int min = ReadInt("Минимальное число: ");
+int max = ReadInt("Максимальное число: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int maxFillarray = numbers[0];
+int minFillarray = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxFillarray)
+    {
+        maxFillarray = numbers[i];
+    }
+    if (numbers[i] < minFillarray)
+    {
+        minFillarray = numbers[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным числом = {maxFillarray - minFillarray}");
+
+void FillArrayRandomNumbers(int[] array) //Заполнение массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array) //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)  //Функция ввода
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
