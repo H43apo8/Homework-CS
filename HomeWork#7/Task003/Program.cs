98ec2e932d062efbcf140a73723e1579fb02199c
H43apo8 <H43apo8@yandex.ru>
2022-12-21 14:22:45 +0300

    ДЗ №7.3 "Среднее арифметическое элементов массива в каждом столбце"

---
+++ HomeWork#7/Task003/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine(" Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.\r\n");
+
+Random random = new Random();
+
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
