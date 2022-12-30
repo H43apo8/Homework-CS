bf817fbd6e31fbdd301a46e90924cb481c616f66
H43apo8 <H43apo8@yandex.ru>
2022-12-30 08:49:36 +0300

    ДЗ №9.1 "Натуральные числа в промежутке от N до 1"

---
+++ HomeWork#9/Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
